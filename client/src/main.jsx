23656ccfcc788329d1c7084adc5f38526805739f
Mbarak-jr <mohammedmbarak77@gmail.com>
2025-07-25 20:34:22 +0300

    added sentry into the client side too for tracking errors in real time

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import BugProvider from './providers/BugProvider' // ✅ correct now
+import BugProvider from './providers/BugProvider'
 import { BrowserRouter } from 'react-router-dom'
+import * as Sentry from '@sentry/react'
+
+// Initialize Sentry as early as possible
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  environment: import.meta.env.MODE, // 'development' or 'production'
+  tracesSampleRate: 1.0,
+  sendDefaultPii: true,
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
